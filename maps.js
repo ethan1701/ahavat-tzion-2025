8476a68e3c8f29defa17ab520eb99277446f0c2a
Ethan Shalev <shalev.ethan@gmail.com>
2024-09-16 02:02:27 +0300

    Update maps.js

--- maps.js
+++ maps.js
@@ -8,41 +8,21 @@
 
 // });
 
-function drawMap(markers='{"addresses":[{"title":"nowhere","latlng":"0, 0"}]}') {
+function drawMap(markers='{"addresses":[{"title":"nowhere","latlng":{"lat": 0, "lng": 0}}]}') {
 	markers = JSON.parse(markers);
 //	console.log(markers);
-	
-    var map;
-    var bounds = new google.maps.LatLngBounds();
-    var mapOptions = {
-        mapTypeId: 'roadmap'
-    };
-                    
-    // Display a map on the page
-    map = new google.maps.Map(document.getElementById("GoogleMap"), mapOptions);
-    map.setTilt(45);
-                         
-    // Display multiple markers on a map
-    var infoWindow = new google.maps.InfoWindow(), marker, i;
-    
-    // Loop through our array of markers & place each one on the map  
-    for( i = 0; i < markers.addresses.length; i++ ) {
-		var position = new google.maps.LatLng(markers.addresses[i].latlng);
-        bounds.extend(position);
-        marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: markers.addresses[i].title
-        });
-        
-        // Automatically center the map fitting all markers on the screen
-        map.fitBounds(bounds);
-    }
 
-    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-        this.setZoom(15);
-        google.maps.event.removeListener(boundsListener);
+  const position = markers.addresses[0].latlng;
+
+  const map = new google.maps.Map(document.getElementById("GoogleMap"),  {
+    zoom: 15,
+    center: position,
+    mapId: "DEMO_MAP_ID", // Map ID is required for advanced markers.
+  });
+
+    const marker = new google.maps.marker.AdvancedMarkerElement({
+        map,
+        position: position,
+        title: markers.addresses[0].title,
     });
-    
 } 
